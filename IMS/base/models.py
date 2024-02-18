421f2384d2ab26d1a44e49e7d7b23cea8a4ba91e
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-18 20:33:55 +0300

    Создали модели

--- IMS/base/models.py
+++ IMS/base/models.py
@@ -3,4 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    type = models.CharField(max_length=100, blank=False)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+    status = models.CharField(max_length=10, default="SOLID") # Available, Solid, Restocking
+    issues = models.CharField(max_length=10, default="No issues") 
+
+    def __str__(self):
+        return 'Type : {0} Quantity : {1} Price : {2}'.format(self.type, 
+                    self.quantity, self.price)
+    
+    
+class Laptop(Product):
     pass
+
+class Desctop(Product):
+    pass
+
+class Mobile(Product):
+    pass    
