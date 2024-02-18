c734ec2f117dfeae1ed50bb8c62823ec98b6e773
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-18 20:47:38 +0300

    Доделал модель

--- IMS/base/models.py
+++ IMS/base/models.py
@@ -7,8 +7,16 @@ class Product(models.Model):
     quantity = models.IntegerField()
     price = models.IntegerField()
 
-    status = models.CharField(max_length=10, default="SOLID") # Available, Solid, Restocking
+    STATUS_CHOICES = (
+        ('AVAILABLE', 'Item ready to be purchased'),
+        ('SOLD', 'Item sold'),
+        ('RESTOCKING', 'Item restocking in few days'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='SOLD') # Available, Sold, Restocking
     issues = models.CharField(max_length=10, default="No issues") 
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return 'Type : {0} Quantity : {1} Price : {2}'.format(self.type, 
