912858877a82560badcfd0f12c4298d3d0c4872b
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-19 08:35:47 +0300

    Добавил вспомогательную модель Производитель

--- IMS/base/models.py
+++ IMS/base/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    country = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     type = models.CharField(max_length=100, blank=False)
     quantity = models.IntegerField()
     price = models.IntegerField()
