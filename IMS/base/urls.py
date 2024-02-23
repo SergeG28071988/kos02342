8eb6746d126b81d29428ebd74157f3d483fb92c0
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-23 14:26:09 +0300

    Создал форму для добавления

--- IMS/base/urls.py
+++ IMS/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import manufacturer_list, product_list
+from .views import manufacturer_list, product_list, display_manufacturers, display_laptops, display_desktops, display_mobiles
 
 
 urlpatterns = [
     path('manufacturer_list/', manufacturer_list, name='manufacturer_list'),
+    path('display_manufacturers/', display_manufacturers, name='display_manufacturers'),
     path('product_list/', product_list, name='product_list'),
+    path('display_laptops/',display_laptops, name='display_laptops'),
+    path('display_desktops/', display_desktops, name='display_desktops'),
+    path('display_mobiles/', display_mobiles, name='display_mobiles'),    
 ]
--- IMS/base/views.py
+++ IMS/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -8,3 +9,44 @@ def manufacturer_list(request):
 
 def product_list(request):
     return render(request, 'product_list.html')
+
+
+def display_manufacturers(request):
+    manufacturers = Manufacturer.objects.all()
+
+    context = {
+        'manufacturers': manufacturers,
+        'header': 'Manufacturer' 
+    }   
+    
+    return render(request, 'manufacturer_list.html', context)
+
+
+def display_laptops(request):
+    items = Laptop.objects.all()
+    context = {
+        'items': items,
+        'header': 'Laptop'
+    }   
+    
+    return render(request, 'product_list.html', context)
+    
+
+def display_desktops(request):
+    items = Desktop.objects.all()
+    context = {
+        'items': items,
+        'header': 'Desktop'
+    }   
+    
+    return render(request, 'product_list.html', context)
+
+
+def display_mobiles(request):
+    items = Mobile.objects.all()
+    context = {
+        'items': items,
+        'header': 'Mobile'
+    }   
+    
+    return render(request, 'product_list.html', context)
