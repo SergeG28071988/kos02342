7986bb889ec1116e6d2b6e3ed119d45178a48bc1
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-24 17:04:32 +0300

    Проект готов!

--- IMS/base/urls.py
+++ IMS/base/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .import views
+from .views import CustomLoginView, RegisterPage
+from .views import logout_view
 
 urlpatterns = [
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    # Маршруты 
+    path('', views.index, name='index'), # общий маршрут
+
     path('manufacturer_list/', views.manufacturer_list, name='manufacturer_list'),
     path('display_manufacturers/', views.display_manufacturers, name='display_manufacturers'),
     path('add_manufacturer/', views.add_manufacturer, name='add_manufacturer'), 
--- IMS/base/views.py
+++ IMS/base/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.urls import reverse_lazy
+from django.views.generic.edit import  FormView
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+    
+
+class RegisterPage(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegisterPage, self).get(*args, **kwargs)
+    
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+def index(request):
+     context = {
+         'title': 'Главная страница сайта' 
+         }
+     return render(request, 'index.html', context)
+
 
 def manufacturer_list(request):
     return render(request, 'manufacturer_list.html')
